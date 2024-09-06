cfe69de70feae798dc12c799a7a79a8c949d9d79
Jeff Sokol <JeffSokolDev@gmail.com>
2024-09-06 10:26:59 -0400

    styling update

--- app/dashboard/transactions/page.js
+++ app/dashboard/transactions/page.js
@@ -283,13 +283,13 @@ export default function Page() {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="sm:flex sm:items-center">
+                                <div className="flex items-center justify-center">
                                     <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
                                         <Link
                                             href="/dashboard/transactions/create"
-                                            className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                                            className="flex h-10 items-center justify-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
                                         >
-                                            <span className="hidden md:block">Create Transaction</span>
+                                            <span className='text-align: center'> Create Transaction</span>
                                         </Link>
                                     </div>
                                 </div>
