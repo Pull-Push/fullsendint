95da80d985207503275193a567aa3490abd6ea94
Jeff Sokol <JeffSokolDev@gmail.com>
2024-09-06 09:25:44 -0400

    beta version

--- app/dashboard/transactions/page.js
+++ app/dashboard/transactions/page.js
@@ -29,14 +29,13 @@ const userNavigation = [
 ]
 const { rows } = await sql`SELECT * FROM transactions JOIN customers ON transactions.customer_id = customers.id ORDER BY transactions.date DESC LIMIT 6`;
 
-
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
+
 // new below this line
 
 export default function Page() {
-
     return (
         <>
             <div className="min-h-full">
@@ -171,8 +170,7 @@ export default function Page() {
                                     <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                                         <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
                                             {/* <Search placeholder="Search transactions..." /> */}
-
-                                            <table id='trans-table'  className="min-w-full divide-y divide-gray-300">
+                                            <table id='trans-table' className="min-w-full divide-y divide-gray-300">
                                                 <thead>
                                                     <tr>
                                                         <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
@@ -210,9 +208,9 @@ export default function Page() {
                                                             <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
                                                                 {transaction.name}
                                                             </td>
-                                                            {/* <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                                                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
                                                                 {transaction.name}
-                                                            </td> */}
+                                                            </td>
                                                             <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{transaction.amount}</td>
                                                             <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{transaction.status}</td>
                                                             <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{transaction.date.toISOString().split('T')[0]}</td>
@@ -243,15 +241,14 @@ export default function Page() {
                                                 <div className="hidden md:-mt-px md:flex">
                                                     <a
                                                         href="#"
-                                                        className="inline-flex items-center border-t-2 border-transparent px-4 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                                                        aria-current="page"
+                                                        className="inline-flex items-center border-t-2 border-indigo-500 px-4 pt-4 text-sm font-medium text-indigo-600"
                                                     >
                                                         1
                                                     </a>
-                                                    {/* Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300" */}
                                                     <a
                                                         href="#"
-                                                        aria-current="page"
-                                                        className="inline-flex items-center border-t-2 border-indigo-500 px-4 pt-4 text-sm font-medium text-indigo-600"
+                                                        className="inline-flex items-center border-t-2 border-transparent px-4 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700 "
                                                     >
                                                         2
                                                     </a>
